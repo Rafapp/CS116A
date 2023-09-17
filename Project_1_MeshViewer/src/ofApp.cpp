3f765a72676e01b4145cd0cc24d7869a7c69d06b
Rafagamedev <rpadiper@gmail.com>
2023-09-16 22:56:16 -0700

    Finished mesh visualization

--- Project_1_MeshViewer/src/ofApp.cpp
+++ Project_1_MeshViewer/src/ofApp.cpp
@@ -1,13 +1,102 @@
 #include "ofApp.h"
 
+Mesh mesh("geo\\cube.obj");
+
+#pragma region Mesh
+Mesh::Mesh() { createTestShape(); }
+
+Mesh::Mesh(string path) : path(path) {};
+
+vector<string> split(const string& input, char delimeter) {
+	vector<string> values;
+	string value;
+	stringstream ss(input);
+
+	while (getline(ss, value, delimeter)) {
+		values.push_back(value);
+	}
+	return values;
+}
+
+void Mesh::setup() {
+
+	verts.push_back(glm::vec3(0, 0, 0));
+
+	cout << "Opening: " << ofToDataPath(path) << " ..." << endl;
+	ofBuffer buffer = ofBufferFromFile(ofToDataPath(path));
+
+	for (string line : buffer.getLines()) {
+
+		if (line.empty()) continue;
+
+		if (line[1] == ' ') {
+			vector<float> v; // Verts
+			vector<int> f; // Faces
+			switch (line[0]) {
+				case 'v':
+					for (string value : split(line, ' ')) {
+						// ~TODO: Fix this it's super dirty!
+						try {
+							v.push_back(stof(value));
+						}
+						catch (const std::exception e) {} 
+					}
+					verts.push_back(glm::vec3(v[0], v[1], v[2]));
+					break;
+				case 'f':
+					for (string face : split(line, ' ')) {
+						// ~TODO: Fix this, it's super dirty!
+						try {
+							f.push_back(stoi(split(face, '/')[0]));
+						}
+						catch (const std::exception e) {}
+					}
+					// Works for any n-gon
+					for (int i = 0; i < f.size() - 2; i++) {
+						tris.push_back(glm::vec3(f[i], f[i + 1], f[i + 2]));
+					}
+					break;
+				}
+			continue;
+		}
+	}
+}
+
+void Mesh::createTestShape() {
+	verts.push_back(glm::vec3(0, 10, 0));
+	verts.push_back(glm::vec3(10, 0, 0));
+	verts.push_back(glm::vec3(0, 0, 10));
+	verts.push_back(glm::vec3(-10, 0, 0));
+	verts.push_back(glm::vec3(0, 0, -10));
+
+	tris.push_back(glm::vec3(1, 0, 2));
+	tris.push_back(glm::vec3(2, 0, 3));
+	tris.push_back(glm::vec3(3, 0, 4));
+	tris.push_back(glm::vec3(4, 0, 1));
+	tris.push_back(glm::vec3(1, 4, 2));
+	tris.push_back(glm::vec3(2, 4, 3));
+}
+
+void Mesh::draw() {
+	for (glm::vec3 tri : tris) {
+		ofDrawTriangle(verts[tri.x], verts[tri.y], verts[tri.z]);
+	}
+}
+
+
+#pragma endregion
+
+#pragma region OF
 //--------------------------------------------------------------
 void ofApp::setup(){
+	// Camera
 	ofSetBackgroundColor(ofColor::black);
 	cam.setDistance(10);
 	cam.setNearClip(.1);
 	ofEnableDepthTest();
 
-	
+	// Mesh
+	mesh.setup();
 }
 
 //--------------------------------------------------------------
@@ -26,7 +115,7 @@ void ofApp::draw(){
 	ofDrawGridPlane();
 	ofPopMatrix();
 
-	// Drawing
+	// Draw mesh
 	ofSetColor(ofColor::white);
 	ofNoFill();
 	mesh.draw();
@@ -88,3 +177,4 @@ void ofApp::gotMessage(ofMessage msg){
 void ofApp::dragEvent(ofDragInfo dragInfo){ 
 
 }
+#pragma endregion
--- Project_1_MeshViewer/src/ofApp.h
+++ Project_1_MeshViewer/src/ofApp.h
@@ -1,52 +1,30 @@
 #pragma once
 
 #include "ofMain.h"
-
 #include <iostream>
 using namespace std;
 
 /*
  * Mesh class
  */
-
 class Mesh {
 
-public:
+	public:
 
-	// Vector of vertices < <x,y,z>>, <x,y,z>, <x,y,z> >
-	vector<glm::vec3> verts;
+		// Vector of vertices < <x,y,z>>, <x,y,z>, <x,y,z> >
+		vector<glm::vec3> verts;
 
-	// Vector of triangles <v1, v2, v3>
-	vector<glm::vec3> tris;
+		// Vector of triangles <v1, v2, v3>
+		vector<glm::vec3> tris;
 
-	// Constructor for testing
-	Mesh() {
-		createPyramid();
-	}
+		string path;
 
-	// Test pyramid
-	void createPyramid() {
-		verts.push_back(glm::vec3(0, 10, 0));
-		verts.push_back(glm::vec3(10, 0, 0));
-		verts.push_back(glm::vec3(0, 0, 10));
-		verts.push_back(glm::vec3(-10, 0, 0));
-		verts.push_back(glm::vec3(0, 0, -10));
+		Mesh();
+		Mesh(string path);
 
-		tris.push_back(glm::vec3(1, 0, 2));
-		tris.push_back(glm::vec3(2, 0, 3));
-		tris.push_back(glm::vec3(3, 0, 4));
-		tris.push_back(glm::vec3(4, 0, 1));
-		tris.push_back(glm::vec3(1, 4, 2));
-		tris.push_back(glm::vec3(2, 4, 3));
-	}
-
-	// Draw method
-	void draw() {
-		for (glm::vec3 tri : tris) {
-			ofDrawTriangle(verts[tri[0]], verts[tri[1]], verts[tri[2]]);
-		}
-	}
-
+		void setup();
+		void createTestShape();
+		void draw();
 };
 
 /*
@@ -74,6 +52,5 @@ class ofApp : public ofBaseApp{
 
 		// Objects
 		ofEasyCam cam;
-		Mesh mesh;
 };
 
