d81eb131be6ec9e37013096292418dfe4839196d
Rafagamedev <rpadiper@gmail.com>
2023-09-19 20:51:27 -0700

    Update files

--- Project_1_MeshViewer/src/ofApp.cpp
+++ Project_1_MeshViewer/src/ofApp.cpp
@@ -1,7 +1,7 @@
 #include "ofApp.h"
 
 
-Mesh mesh("geo/teapot.obj");
+Mesh mesh("geo/Woob.obj");
 
 #pragma region Mesh
 Mesh::Mesh() { createTestShape(); }
@@ -24,7 +24,7 @@ void Mesh::setup() {
     verts.push_back(glm::vec3(0, 0, 0));
 
     cout << "Loading mesh " << ofToDataPath(path) << "..." << endl;
-    
+
     ofBuffer buffer = ofBufferFromFile(ofToDataPath(path));
 
     int vCount = 0;
@@ -37,49 +37,49 @@ void Mesh::setup() {
             vector<float> v; // Verts
             vector<int> f; // Faces
             switch (line[0]) {
-                case 'v':
-                    for (string value : split(line, ' ')) {
-                        // ~TODO: Fix this it's super dirty!
-                        try {
-                            v.push_back(stof(value));
-                        }
-                        catch (const std::exception e) {}
+            case 'v':
+                for (string value : split(line, ' ')) {
+                    // ~TODO: Fix this it's super dirty!
+                    try {
+                        v.push_back(stof(value));
                     }
-                    verts.push_back(glm::vec3(v[0], v[1], v[2]));
-                    vCount++;
-                    cout << "\r" << string(50, ' ') << "\rLoaded vertices : " << vCount << flush;
-                    break;
-                case 'f':
-                    for (string face : split(line, ' ')) {
-                        // ~TODO: Fix this, it's super dirty!
-                        try {
-                            f.push_back(stoi(split(face, '/')[0]));
-                        }
-                        catch (const std::exception e) {}
-                    }
-                    // Works for any n-gon
-//                    for (int i = 0; i < f.size() - 2; i++) {
-                        tris.push_back(glm::vec3(f[0], f[1], f[2]));
-                        tris.push_back(glm::vec3(f[0], f[2], f[3]));
-                        fCount++;
-//                        cout << "\r" << string(50, ' ') << "\rLoaded faces: " << fCount << flush;
-//                    }
-                    break;
+                    catch (const std::exception e) {}
                 }
+                verts.push_back(glm::vec3(v[0], v[1], v[2]));
+                vCount++;
+                cout << "\r" << string(50, ' ') << "\rLoaded vertices : " << vCount << flush;
+                break;
+            case 'f':
+                for (string face : split(line, ' ')) {
+                    // ~TODO: Fix this, it's super dirty!
+                    try {
+                        f.push_back(stoi(split(face, '/')[0]));
+                    }
+                    catch (const std::exception e) {}
+                }
+                // Works for any n-gon
+//                    for (int i = 0; i < f.size() - 2; i++) {
+                tris.push_back(glm::vec3(f[0], f[1], f[2]));
+                tris.push_back(glm::vec3(f[0], f[2], f[3]));
+                fCount++;
+                //                        cout << "\r" << string(50, ' ') << "\rLoaded faces: " << fCount << flush;
+                //                    }
+                break;
+            }
             continue;
         }
     }
     cout << "\r" << string(50, ' ') << "\r";
     cout << "Finished loading model : " << ofToDataPath(path) << endl << flush;
-    cout << endl <<  "---STATS---" << endl;
+    cout << endl << "---STATS---" << endl;
     cout << "Vertices: " << vCount << endl;
     cout << "Triangles: " << fCount << endl;
 
     // Vert count * 3 coords (x,y,z) * 4 bytes (float)
-    cout << "Vertex memory: " << (vCount * 3 * 4)/1000. << " kb" << endl;
+    cout << "Vertex memory: " << (vCount * 3 * 4) / 1000. << " kb" << endl;
 
     // Tri count * 3 indices (v1,v2,v3) * 4 bytes (int)
-    cout << "Triangle memory: " << (fCount * 3 * 4)/1000. << " kb" << endl;
+    cout << "Triangle memory: " << (fCount * 3 * 4) / 1000. << " kb" << endl;
 
     cout << "Total memory: " << ((vCount * 3 * 4) / 1000. + (fCount * 3 * 4) / 1000.) << " kb" << endl;
 
@@ -117,7 +117,7 @@ void Mesh::drawSelected(vector<glm::vec3> tris) {
     for (glm::vec3 tri : tris) {
         ofDrawTriangle(verts[tri.x], verts[tri.y], verts[tri.z]);
     }
-    
+
     ofSetColor(ofColor::white);
     ofNoFill();
     for (glm::vec3 tri : tris) {
@@ -135,17 +135,17 @@ void Mesh::drawAdjacentTriangles() {
         glm::vec3 triIndexes = tris[selectedIndex];
         for (glm::vec3 tri : tris) {
             int contained = 0;
-            for(int i = 0; i < 3; i++){
-                if(triIndexes.x == tri[i] || triIndexes.y == tri[i] || triIndexes.z == tri[i]) contained ++;
-                if(contained >= 2){
+            for (int i = 0; i < 3; i++) {
+                if (triIndexes.x == tri[i] || triIndexes.y == tri[i] || triIndexes.z == tri[i]) contained++;
+                if (contained >= 2) {
                     temp.push_back(tri);
                     cout << "BREAK: " << contained << endl;
                     break;
                 }
             }
-//            if ((tri.x == triIndexes.x && tri.y == triIndexes.y) || (tri.y == triIndexes.y && tri.z == triIndexes.z) || (tri.z == triIndexes.z && tri.x == triIndexes.x)) {
-//                temp.push_back(tri);
-//            }
+            //            if ((tri.x == triIndexes.x && tri.y == triIndexes.y) || (tri.y == triIndexes.y && tri.z == triIndexes.z) || (tri.z == triIndexes.z && tri.x == triIndexes.x)) {
+            //                temp.push_back(tri);
+            //            }
         }
         mesh.drawSelected(temp);
     }
@@ -169,7 +169,7 @@ void Mesh::drawAdjacentTriangles() {
 
 #pragma region OF
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
     // Camera
     ofSetBackgroundColor(ofColor::black);
     cam.setDistance(10);
@@ -187,12 +187,12 @@ void ofApp::setup(){
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
     cam.begin();
 
     // Grid
@@ -229,57 +229,57 @@ void ofApp::draw(){
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::keyPressed(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::mouseExited(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
+void ofApp::gotMessage(ofMessage msg) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
 #pragma endregion
--- Project_1_MeshViewer/src/ofApp.h
+++ Project_1_MeshViewer/src/ofApp.h
@@ -10,66 +10,66 @@ using namespace std;
  */
 class Mesh {
 
-    public:
+public:
 
-        // Vector of vertices < <x,y,z>>, <x,y,z>, <x,y,z> >
-        vector<glm::vec3> verts;
+    // Vector of vertices < <x,y,z>>, <x,y,z>, <x,y,z> >
+    vector<glm::vec3> verts;
 
-        // Vector of triangles <v1, v2, v3>
-        vector<glm::vec3> tris;
+    // Vector of triangles <v1, v2, v3>
+    vector<glm::vec3> tris;
 
-        // Size (calculated after setup)
-        int triCount = 0;
-        int vertCount = 0;
+    // Size (calculated after setup)
+    int triCount = 0;
+    int vertCount = 0;
 
-        bool drawAdjacent = false;
-        char selectedType = ' ';
-        int selectedIndex = 0;
+    bool drawAdjacent = false;
+    char selectedType = ' ';
+    int selectedIndex = 0;
 
-        string path;
+    string path;
 
-        Mesh();
-        Mesh(string path);
+    Mesh();
+    Mesh(string path);
 
-        void setup();
-        void createTestShape();
-        void draw();
-        void drawSelected(vector<glm::vec3> tris);
-        void drawAdjacentTriangles();
+    void setup();
+    void createTestShape();
+    void draw();
+    void drawSelected(vector<glm::vec3> tris);
+    void drawAdjacentTriangles();
 };
 
 /*
  * App class
  */
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp {
 
-    public:
-        void setup();
-        void update();
-        void draw();
+public:
+    void setup();
+    void update();
+    void draw();
 
-        void keyPressed(int key);
-        void keyReleased(int key);
-        void mouseMoved(int x, int y );
-        void mouseDragged(int x, int y, int button);
-        void mousePressed(int x, int y, int button);
-        void mouseReleased(int x, int y, int button);
-        void mouseEntered(int x, int y);
-        void mouseExited(int x, int y);
-        void windowResized(int w, int h);
-        void dragEvent(ofDragInfo dragInfo);
-        void gotMessage(ofMessage msg);
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y);
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
 
-        // Variables
-        
-        
-        // Objects
-        ofEasyCam cam;
-        ofxPanel gui;
-        ofxLabel label;
-        ofxIntField numberField;
-        ofxButton vertexBtn;
-        ofxButton triangleBtn;
+    // Variables
+
+
+    // Objects
+    ofEasyCam cam;
+    ofxPanel gui;
+    ofxLabel label;
+    ofxIntField numberField;
+    ofxButton vertexBtn;
+    ofxButton triangleBtn;
 };
 
